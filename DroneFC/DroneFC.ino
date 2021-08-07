d99b5e7e7c0ca20e4e39845e336be0b0135208bf
SeugnshinLee <lsver67@gmail.com>
2021-08-07 15:34:55 +0900

    기존코드 작동하지 않아, 전반적으로 수정. 1 부분: AcX, AcY 변수 사용 하지 않음 -> 삭제, 함수 정의 추가 2 부분: int sendata()  제거된 함수 -> 삭제 3 부분: 사용되지 않은 전역 변수 제거, 제어기 전역변수 위치 수정 3 부분 89줄: Serial1 통신속도 115200 -> 9600 수정, 통신 성공, IMU 센서 시작 코드 삽입. 4 부분: int calcAccelYRP 등 2개 -> calcFilteredYPR() 함수에 기능 통합 4. 초기 각도값 base_~~~_target_angle = 0 으로 초기화 6. calcGyroYPR() 에서 GYROXYZ_TO_DEGREES_PER_SEC; 삭제 -> IMU.read 시 다 계산되어 나옴.

--- DroneFC/DroneFC.ino
+++ DroneFC/DroneFC.ino
@@ -1,16 +1,20 @@
 #include <Arduino_LSM9DS1.h>
 
-const int MPU_addr = 0x68;
-int16_t AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ;
-
 int calibAccelGyro();
 int initDT();
 int calcDT();
-int senddata();
-int calcAccelYPR();
-int calcGyroYPR();
 int calcFilteredYPR();
 int initYPR();
+void SendDataToProcessing();
+void calcYPRtoDualPID();
+void calcMotorSpeed();
+void checkMspPacket();
+void updateMotorSpeed();
+void printMspPacket();
+
+
+// 수신기 데이터값
+uint8_t mspPacket[11];
 
 // 센서 초기값
 float baseAcX, baseAcY, baseAcZ;
@@ -21,6 +25,11 @@ float accel_x, accel_y, accel_z;
 float gyro_x, gyro_y, gyro_z;
 float mag_x, mag_y, mag_z;
 
+// 초기각도 값
+float base_roll_target_angle = 0;
+float base_pitch_target_angle = 0;
+float base_yaw_target_angle = 0;
+
 // dt
 unsigned long t_now;
 unsigned long t_prev;
@@ -28,20 +37,62 @@ float dt = 0.0;
 
 // 오일러각
 float accel_angle_x, accel_angle_y, accel_angle_z;
-float gyro_angle_x, gyro_angle_y, gyro_angle_z;
-
 // 상보필터로 구한 각 = 최종각
 float filtered_angle_x, filtered_angle_y, filtered_angle_z;
 
-// 제어 기준값
-float init_angle_x, init_angle_y, init_angle_z;
+// 제어값
+float roll_target_angle = 0.0;
+float roll_angle_in;
+float roll_rate_in;
+float roll_stabilize_kp = 1;
+float roll_stabilize_ki = 0;
+float roll_rate_kp = 1;
+float roll_rate_ki = 0;
+float roll_stabilize_iterm;
+float roll_rate_iterm;
+float roll_output;
+
+float pitch_target_angle = 0.0;
+float pitch_angle_in;
+float pitch_rate_in;
+float pitch_stabilize_kp = 1;
+float pitch_stabilize_ki = 0;
+float pitch_rate_kp = 1;
+float pitch_rate_ki = 0;
+float pitch_stabilize_iterm;
+float pitch_rate_iterm;
+float pitch_output;
+
+float yaw_target_angle = 0.0;
+float yaw_angle_in;
+float yaw_rate_in;
+float yaw_stabilize_kp = 1;
+float yaw_stabilize_ki = 0;
+float yaw_rate_kp = 1;
+float yaw_rate_ki = 0;
+float yaw_stabilize_iterm;
+float yaw_rate_iterm;
+float yaw_output;
+
+float throttle = 0.0;
+float motorA_speed, motorB_speed, motorC_speed, motorD_speed;
+#define THROTTLE_MAX 255
+#define THROTTLE_MIN 0
+
+const int motorA_pin = 8;
+const int motorB_pin = 9;
+const int motorC_pin = 7;
+const int motorD_pin = 12;
 
 void setup() {
   Serial.begin(115200);
-  Serial1.begin(115200);
+  Serial1.begin(9600);
+  if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+    while (1);
+  }
   calibAccelGyro();
   initDT();
-  //accelNoiseTest();
   initYPR();
 }
 
@@ -50,111 +101,108 @@ void loop() {
   IMU.readGyroscope(gyro_x, gyro_y, gyro_z);
   IMU.readMagneticField(mag_x, mag_y, mag_z);
   calcDT();
-  calcAccelYPR();
-  calcGyroYPR();
   calcFilteredYPR();
 
   calcYPRtoDualPID();
   calcMotorSpeed();
   checkMspPacket();
   updateMotorSpeed();
+  SendDataToProcessing();
 
-  //static int cnt;
-  //cnt++;
-  //if(cnt%2 == 0) {
-  //SendDataToProcessing();
 }
 
-extern float roll_output, pitch_output, yaw_output;
-extern float motorA_speed, motorB_speed, motorC_speed, motorD_speed;
+void SendDataToProcessing() {
+  Serial.print("DEL: ");
+  Serial.print(dt, DEC);
+  Serial.print("#RPY: ");
+  Serial.print(filtered_angle_y, 2);
+  Serial.print(",");
+  Serial.print(filtered_angle_x, 2);
+  Serial.print(",");
+  Serial.print(filtered_angle_z, 2);
 
-//void SendDataToProcessing() {
-//Serial.print("DEL: ");
-//Serial.print(dt, DEC);
-//Serial.print("#RPY: ");
-//Serial.print(filtered_angle_y, 2);
-//Serial.print(",");
-//Serial.print(filtered_angle_x, 2);
-//Serial.print(",");
-//Serial.print(filtered_angle_z, 2);
-//Serial.print("#PID");
-//Serial.print(roll_output, 2);
-//Serial.print(",");
-//Serial.print(pitch_output, 2);
-//Serial.print(",");
-//Serial.print(yaw_output, 2);
-//Serial.print("#A");
-//Serial.print(motorA_speed, 2);
-//Serial.print("#B");
-//Serial.print(motorB_speed, 2);
-//Serial.print("#C");
-//Serial.print(motorC_speed, 2);
-//Serial.print("#D");
-//Serial.print(motorD_speed, 2);
-//}
+  Serial.print("#TARGET: ");
+  Serial.print(roll_target_angle, 2);
+  Serial.print(",");
+  Serial.print(pitch_target_angle, 2);
+  Serial.print(",");
+  Serial.print(yaw_target_angle, 2);
+  Serial.print(",");
+  Serial.print(throttle, 2);
+
+  Serial.print("#A");
+  Serial.print(motorA_speed, 2);
+  Serial.print("#B");
+  Serial.print(motorB_speed, 2);
+  Serial.print("#C");
+  Serial.print(motorC_speed, 2);
+  Serial.print("#D");
+  Serial.println(motorD_speed, 2);
+}
 
 int calibAccelGyro() {
   float sumAcX = 0, sumAcY = 0, sumAcZ = 0;
   float sumGyX = 0, sumGyY = 0, sumGyZ = 0;
 
-  for (int i = 0; i < 10; i++) {
+  for (int i = 0; i < 100; i++) {
     IMU.readAcceleration(accel_x, accel_y, accel_z);
     IMU.readGyroscope(gyro_x, gyro_y, gyro_z);
     sumAcX += accel_x, sumAcY += accel_y, sumAcZ += accel_z;
     sumGyX += gyro_x, sumGyY += gyro_y, sumGyZ += gyro_z;
-    delay(100);
+    delay(10);
   }
 
-  baseAcX = sumAcX / 10;
-  baseAcY = sumAcY / 10;
-  baseAcZ = sumAcZ / 10;
+  baseAcX = sumAcX / 100;
+  baseAcY = sumAcY / 100;
+  baseAcZ = sumAcZ / 100;
 
-  baseGyX = sumGyX / 10;
-  baseGyY = sumGyY / 10;
-  baseGyZ = sumGyZ / 10;
+  baseGyX = sumGyX / 100;
+  baseGyY = sumGyY / 100;
+  baseGyZ = sumGyZ / 100;
+
+  return 0;
 }
 
 int initDT() {
   t_prev = micros();
+
+  return 0;
 }
 
 int calcDT() {
   t_now = micros();
   dt = (t_now - t_prev) / 1000000.0;
   t_prev = t_now;
+
+  return 0;
 }
 
-int calcAccelYPR() {
-  float accel_x, accel_y, accel_z;
-  float accel_xz, accel_yz;
-  const float RADIANS_TO_DEGREES = 180 / 3.14159;
-
-  accel_x = AcX - baseAcX;
-  accel_y = AcY - baseAcY;
-  accel_z = AcZ + (16384 - baseAcZ);
-
-  accel_yz = sqrt(pow(accel_y, 2) + pow(accel_z, 2));
-  accel_angle_y = atan(-accel_x / accel_yz) * RADIANS_TO_DEGREES;
-
-  accel_xz = sqrt(pow(accel_x, 2) + pow(accel_z, 2));
-  accel_angle_x = atan(accel_y / accel_xz) * RADIANS_TO_DEGREES;
-
-  accel_angle_z = 0;
-}
-
-int calcGyroYPR() {
-  const float GYROXYZ_TO_DEGREES_PER_SEC = 131;
-
-  gyro_x = (GyX - baseGyX) / GYROXYZ_TO_DEGREES_PER_SEC;
-  gyro_y = (GyY - baseGyY) / GYROXYZ_TO_DEGREES_PER_SEC;
-  gyro_z = (GyZ - baseGyZ) / GYROXYZ_TO_DEGREES_PER_SEC;
-
-  //gyro_angle_x += gyro_x = dt;
-  //gyro_angle_y += gyro_y = dt;
-  //gyro_angle_z += gyro_z = dt;
-}
 
 int calcFilteredYPR() {
+  
+  // 가속도 YPR 값 구하기
+  float _accel_x, _accel_y, _accel_z;
+  float _accel_xz, _accel_yz;
+  const float RADIANS_TO_DEGREES = 180 / 3.14159;
+
+  _accel_x = accel_x - baseAcX;
+  _accel_y = accel_y - baseAcY;
+  _accel_z = accel_z + (1 - baseAcZ);
+
+  _accel_yz = sqrt(pow(_accel_y, 2) + pow(_accel_z, 2));
+  accel_angle_y = atan(-_accel_x / _accel_yz) * RADIANS_TO_DEGREES;
+
+  _accel_xz = sqrt(pow(_accel_x, 2) + pow(_accel_z, 2));
+  accel_angle_x = atan(_accel_y / _accel_xz) * RADIANS_TO_DEGREES;
+
+  accel_angle_z = 0;
+  
+  // 자이로 센서 보정값 적용
+  gyro_x = gyro_x - baseGyX;
+  gyro_y = gyro_y - baseGyY;
+  gyro_z = gyro_z - baseGyZ;
+
+  // 상보필터
   const float ALPHA = 0.96;
   float tmp_angle_x, tmp_angle_y, tmp_angle_z;
 
@@ -167,69 +215,10 @@ int calcFilteredYPR() {
   filtered_angle_y =
     ALPHA * tmp_angle_y + (1.0 - ALPHA) * accel_angle_y;
   filtered_angle_z = tmp_angle_z;
+
+  return 0;
 }
 
-//void accelNoiseTest() {
-//analogWrite(6, 40);
-//analogWrite(10, 40);
-//analogWrite(9, 40);
-//analogWrite(5, 40);
-//}
-
-//void stdPID(float& setpoint,
-            //float& input,
-            //float& prev_input,
-            //float& kp,
-            //float& ki,
-            //float& kd,
-            //float& iterm,
-            //float& output) {
-  //float error;
-  //float dInput;
-  //float pterm, dterm;
-
-  //error = setpoint - input;
-  //dInput = input - prev_input;
-  //prev_input = input;
-
-  //pterm = kp * error;
-  //iterm += ki * error * dt;
-  //dterm = -kd * dInput / dt;
-
-  //output = pterm + iterm + dterm;
-//}
-
-//float roll_target_angle = 0.0;
-//float roll_prev_angle = 0.0;
-//float roll_kp = 0.408;
-//float roll_ki = 1.02;
-//float roll_kd = 0.0408;
-//float roll_iterm;
-//float roll_output;
-
-//float pitch_target_angle = 0.0;
-//float pitch_prev_angle = 0.0;
-//float pitch_kp = 0.408;
-//float pitch_ki = 1.02;
-//float pitch_kd = 0.0408;
-//float pitch_iterm;
-//float pitch_output;
-
-//float yaw_target_angle = 0.0;
-//float yaw_prev_angle = 0.0;
-//float yaw_kp = 1;
-//float yaw_ki = 0;
-//float yaw_kd = 0;
-//float yaw_iterm;
-//float yaw_output;
-
-float base_roll_target_angle;
-float base_pitch_target_angle;
-float base_yaw_target_angle;
-
-extern float roll_target_angle;
-extern float pitch_target_angle;
-extern float yaw_target_angle;
 
 int initYPR() {
   for (int i = 0; i < 10; i++) {
@@ -237,8 +226,6 @@ int initYPR() {
     IMU.readGyroscope(gyro_x, gyro_y, gyro_z);
     IMU.readMagneticField(mag_x, mag_y, mag_z);
     calcDT();
-    calcAccelYPR();
-    calcGyroYPR();
     calcFilteredYPR();
 
     base_roll_target_angle += filtered_angle_y;
@@ -246,6 +233,7 @@ int initYPR() {
     base_yaw_target_angle += filtered_angle_z;
 
     delay(100);
+
   }
 
   base_roll_target_angle /= 10;
@@ -255,39 +243,10 @@ int initYPR() {
   roll_target_angle = base_roll_target_angle;
   pitch_target_angle = base_pitch_target_angle;
   yaw_target_angle = base_yaw_target_angle;
+
+  return 0;
 }
 
-//void calcYPRtoStdPID() {
-//  stdPID(roll_target_angle,
-         //filtered_angle_y,
-         //roll_prev_angle,
-         //roll_kp,
-         //roll_ki,
-         //roll_kd,
-         //roll_iterm,
-         //roll_output);
-
-  //stdPID(pitch_target_angle,
-         //filtered_angle_x,
-         //pitch_prev_angle,
-         //pitch_kp,
-         //pitch_ki,
-         //pitch_kd,
-         //pitch_iterm,
-         //pitch_output);
-
-  //stdPID(yaw_target_angle,
-         //filtered_angle_z,
-         //yaw_prev_angle,
-         //yaw_kp,
-         //yaw_ki,
-         //yaw_kd,
-         //yaw_iterm,
-         //yaw_output);
-//}
-
-float throttle = 100.0;
-float motorA_speed, motorB_speed, motorC_speed, motorD_speed;
 
 void calcMotorSpeed() {
   motorA_speed = (throttle == 0) ? 0:
@@ -298,7 +257,7 @@ void calcMotorSpeed() {
     throttle + yaw_output - roll_output - pitch_output;
   motorD_speed = (throttle == 0) ? 0:
     throttle - yaw_output + roll_output - pitch_output;
-  float throttle = 0;
+  //float throttle = 0;
 
   if (motorA_speed < 0) motorA_speed = 0;
   if (motorA_speed > 255) motorA_speed = 255;
@@ -310,55 +269,52 @@ void calcMotorSpeed() {
   if (motorD_speed > 255) motorD_speed = 255;
 }
 
-enum {
-  HEAD1, HEAD2, HEAD3, DATASIZE, CMD,
-  ROLL, PITCH, YAW, THROTTLE,
-  AUX, CRC, PACKETSIZE,
-};
-uint8_t mspPacket[PACKETSIZE];
-
 void checkMspPacket() {
+
   static uint32_t cnt;
 
   if (Serial1.available() > 0) {
     while (Serial1.available() > 0) {
       uint8_t mspData = Serial1.read();
-      if (mspData == '$') cnt = HEAD1;
+      if (mspData == '$') cnt = 0;
       else cnt++;
 
       mspPacket[cnt] = mspData;
 
-      if (cnt == CRC) {
-        if (mspPacket[CMD] == 150) {
-          throttle = mspPacket[THROTTLE];
+      if (cnt == 10) {
+        //printMspPacket();
+        if (mspPacket[4] == 150) {
+          throttle = mspPacket[8];
 
           roll_target_angle = base_roll_target_angle;
           pitch_target_angle = base_pitch_target_angle;
           yaw_target_angle = base_yaw_target_angle;
 
-          roll_target_angle += mspPacket[ROLL] - 125;
-          pitch_target_angle += -(mspPacket[PITCH] - 125);
-          yaw_target_angle += -(mspPacket[YAW] - 125);
-
-          //1. smart throttle
-
-          //2. smart roll
-
-          //3. smart pitch
+          roll_target_angle += (mspPacket[5] - 125);
+          pitch_target_angle += (mspPacket[6] - 125);
+          yaw_target_angle += (mspPacket[7] - 125);
           
         }
       }
     }
   }
 }
+void printMspPacket() {
+  Serial.print(Serial1.available());
+  Serial.print(",");
+  Serial.print((char)mspPacket[0]);
+  Serial.print((char)mspPacket[1]);
+  Serial.print((char)mspPacket[2]);
+  Serial.print(mspPacket[3]);
+  Serial.print(mspPacket[4]);
+  Serial.print(mspPacket[5]);
+  Serial.print(mspPacket[6]);
+  Serial.print(mspPacket[7]);
+  Serial.println(mspPacket[8]);
 
-#define THROTTLE_MAX 255
-#define THROTTLE_MIN 0
+}
 
-int motorA_pin = 6;
-int motorB_pin = 10;
-int motorC_pin = 9;
-int motorD_pin = 5;
+
 
 void initMotorSpeed() {
   analogWrite(motorA_pin, THROTTLE_MIN);
@@ -406,38 +362,6 @@ void dualPID(
     output = rate_pterm + rate_iterm + stabilize_iterm;    
    }
 
-float roll_target_angle = 0.0;
-float roll_angle_in;
-float roll_rate_in;
-float roll_stabilize_kp = 1;
-float roll_stabilize_ki = 0;
-float roll_rate_kp = 1;
-float roll_rate_ki = 0;
-float roll_stabilize_iterm;
-float roll_rate_iterm;
-float roll_output;
-
-float pitch_target_angle = 0.0;
-float pitch_angle_in;
-float pitch_rate_in;
-float pitch_stabilize_kp = 1;
-float pitch_stabilize_ki = 0;
-float pitch_rate_kp = 1;
-float pitch_rate_ki = 0;
-float pitch_stabilize_iterm;
-float pitch_rate_iterm;
-float pitch_output;
-
-float yaw_target_angle = 0.0;
-float yaw_angle_in;
-float yaw_rate_in;
-float yaw_stabilize_kp = 1;
-float yaw_stabilize_ki = 0;
-float yaw_rate_kp = 1;
-float yaw_rate_ki = 0;
-float yaw_stabilize_iterm;
-float yaw_rate_iterm;
-float yaw_output;
 
 void calcYPRtoDualPID() {
 
