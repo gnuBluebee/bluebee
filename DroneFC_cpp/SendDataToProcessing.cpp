e499bc006c446c7a120d1fd6d9c4ddd2bf09b207
SeugnshinLee <lsver67@gmail.com>
2021-08-12 01:24:44 +0900

    FlightData 구조체 적용

--- DroneFC_cpp/SendDataToProcessing.cpp
+++ DroneFC_cpp/SendDataToProcessing.cpp
@@ -1,43 +1,31 @@
 #include <Arduino.h>
+#include "BluebeeDataType.h"
 
-void SendDataToProcessing(
-  float *dt,
-  float *filtered_angle_x,
-  float *filtered_angle_y,
-  float *filtered_angle_z,
-  float *roll_target_angle,
-  float *pitch_target_angle,
-  float *yaw_target_angle,
-  float *throttle,
-  float *motorA_speed,
-  float *motorB_speed,
-  float *motorC_speed,
-  float *motorD_speed
-) {
+void SendDataToProcessing(FlightData *fdata) {
   Serial.print("DEL: ");
-  Serial.print(*dt, DEC);
+  Serial.print(fdata->dt, DEC);
   Serial.print("#RPY: ");
-  Serial.print(*filtered_angle_y, 2);
+  Serial.print(fdata->filtered_angle_y, 2);
   Serial.print(",");
-  Serial.print(*filtered_angle_x, 2);
+  Serial.print(fdata->filtered_angle_x, 2);
   Serial.print(",");
-  Serial.print(*filtered_angle_z, 2);
+  Serial.print(fdata->filtered_angle_z, 2);
 
   Serial.print("#TARGET: ");
-  Serial.print(*roll_target_angle, 2);
+  Serial.print(fdata->roll_target_angle, 2);
   Serial.print(",");
-  Serial.print(*pitch_target_angle, 2);
+  Serial.print(fdata->pitch_target_angle, 2);
   Serial.print(",");
-  Serial.print(*yaw_target_angle, 2);
+  Serial.print(fdata->yaw_target_angle, 2);
   Serial.print(",");
-  Serial.print(*throttle, 2);
+  Serial.print(fdata->throttle, 2);
 
   Serial.print("#A");
-  Serial.print(*motorA_speed, 2);
+  Serial.print(fdata->motorA_speed, 2);
   Serial.print("#B");
-  Serial.print(*motorB_speed, 2);
+  Serial.print(fdata->motorB_speed, 2);
   Serial.print("#C");
-  Serial.print(*motorC_speed, 2);
+  Serial.print(fdata->motorC_speed, 2);
   Serial.print("#D");
-  Serial.println(*motorD_speed, 2);
+  Serial.println(fdata->motorD_speed, 2);
 }
--- DroneFC_cpp/SendDataToProcessing.h
+++ DroneFC_cpp/SendDataToProcessing.h
@@ -1,15 +1,4 @@
+#pragma once
+#include "BluebeeDataType.h"
 
-void SendDataToProcessing(
-  float *dt,
-  float *filtered_angle_x,
-  float *filtered_angle_y,
-  float *filtered_angle_z,
-  float *roll_target_angle,
-  float *pitch_target_angle,
-  float *yaw_target_angle,
-  float *throttle,
-  float *motorA_speed,
-  float *motorB_speed,
-  float *motorC_speed,
-  float *motorD_speed
-  );
+void SendDataToProcessing(FlightData *fdata);
